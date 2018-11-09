1ab0496941aa8ed156b0f12ddd98393a59f59f16
audo23 <audo23@163.com>
2018-11-09 14:40:38 +0800

    test2

---
+++ RunApp.java
@@ -0,0 +1,13 @@
+package com.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//启动类
+@SpringBootApplication
+public class RunApp {
+	//入口函数
+	public static void main(String[] args) {
+		SpringApplication.run(RunApp.class, args);
+	}
+}

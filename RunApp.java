4aa34261554bf40b4bff0c8d9b8091e302b17980
audo23 <36255082+audo23@users.noreply.github.com>
2018-12-18 10:13:56 +0800

    Update RunApp.java

--- RunApp.java
+++ RunApp.java
@@ -3,7 +3,7 @@ package com.tedu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//启动类
+//启动类//201812171013
 @SpringBootApplication
 public class RunApp {
 	//入口函数
